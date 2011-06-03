a58eb2834d2a5c21575f868681475aa8780dc5ef
David Breitkreutz <david.breitkreutz@jcu.edu.au>
2011-06-03 12:08:01 +1000

    New widget for autocomplete keywords

--- tdh/metadata/widgets.py
+++ tdh/metadata/widgets.py
@@ -1,9 +1,13 @@
 import zope.component
-from z3c.form import interfaces
+import zope.interface
+import zope.location
+from z3c.form import action, button, interfaces
 from z3c.form.widget import FieldWidget
+from z3c.form.browser.button import ButtonWidget
 
 from plone.formwidget.autocomplete.widget import \
         AutocompleteMultiSelectionWidget
+
 from collective.z3cform.datagridfield import DataGridField
 
 class AnzrscCodeDataGridField(DataGridField):
@@ -29,8 +33,62 @@ def SeoCodeDataGridFieldFactory(field, request):
     return FieldWidget(field, SeoCodeDataGridField(request))
 
 
+class StandardButtonAction(action.Action, ButtonWidget, zope.location.Location):
+    """Button action class for standard buttons (not submit!)"""
+    zope.interface.implements(interfaces.IButtonAction)
+    zope.component.adapts(interfaces.IFormLayer, interfaces.IButton)
+
+    def __init__(self, request, field):
+        action.Action.__init__(self, request, field.title)
+        ButtonWidget.__init__(self, request)
+        self.field = field
+
+    accesskey = button.ButtonAction.accesskey
+    value = button.ButtonAction.value
+    id = button.ButtonAction.id
+
+def standardButtonActionFactory(request, field):
+    """Factory for producing standard button actions"""
+    button = StandardButtonAction(request, field)
+    import ipdb; ipdb.set_trace()
+    return button
+
+
 class UnrestrictedAutocompleteSelectionWidget(AutocompleteMultiSelectionWidget):
     mustMatch = False
+    newTermButton = u"Add new"
+
+    extra_js = """
+
+    (function($) {
+
+        $().ready(function() {
+            $('#%(id)s-buttons-add').unbind('click').click(function() {
+                field = $('#%(id)s-widgets-query');
+                value = field.val();
+                if (value) {
+                    formwidget_autocomplete_new_value(field, value, value);
+                }
+                return false;
+            });
+        });
+
+    })(jQuery);
+
+    """
+
+
+    def render(self):
+        add_term_button = button.Button("add", title=self.newTermButton)
+        add_term_button.actionFactory = standardButtonActionFactory
+        self.subform.buttons += button.Buttons(add_term_button)
+        self.subform.update()
+
+        position = self.js_template.find("/* ]]> */")
+        self.js_template = self.js_template[:position] + self.extra_js + \
+                self.js_template[position:]
+
+        return super(UnrestrictedAutocompleteSelectionWidget, self).render()
 
 @zope.interface.implementer(interfaces.IFieldWidget)
 def UnrestrictedAutocompleteMultiFieldWidget(field, request):
